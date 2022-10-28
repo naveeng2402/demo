edf3b17ba97f7aab585e22a038debfb8f657e24c
Naveen G <naveeng2402@gmail.com>
2022-10-28 09:55:24 +0530

    added Layout biolerPlate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import { Home, Login } from "./pages";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
