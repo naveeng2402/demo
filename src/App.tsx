7bc3117623137eac32ecb1c49711cd569cc0bf64
Naveen G <naveeng2402@gmail.com>
2022-10-28 09:48:15 +0530

    Added initial structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Home, Login } from "./pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
