12bbfe6a26f57a13aedd01920de2665db73fffed
Naveen G <naveeng2402@gmail.com>
2022-10-28 10:23:02 +0530

    Added logo and colors in tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          300: "#55ceff",
+          500: "#226caa",
+          600: "#094579",
+          700: "#20355d",
+        },
+      },
+    },
   },
   plugins: [],
 };
